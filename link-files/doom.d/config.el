65a6c3e112df62af7e0f9e28cc64b60b0c350371
Ana Daros <anapauladaros@gmail.com>
2021-04-28 23:01:23 -0300

    doom emacs

---
+++ link-files/doom.d/config.el
@@ -0,0 +1,13 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+(setq user-full-name "Ana Daros"
+      user-mail-address "anapauladaros@gmail.com")
+
+(setq doom-theme 'doom-one)
+
+(setq doom-font (font-spec :family "JetBrains Mono" :size 14 :weight 'semi-light)
+      doom-variable-pitch-font (font-spec :family "JetBrains Mono" :size 13))
+
+(setq org-directory "~/org/")
+
+(setq display-line-numbers-type t)
